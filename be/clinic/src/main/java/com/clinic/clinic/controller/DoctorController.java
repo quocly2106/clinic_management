2b27606ac3d1a494f8e68a139c88e2e01084ed7d
quoc <lyhangquoc274@gmail.com>
2024-10-29 15:46:26 +0700

    Quay lại commit 9eeef47 và lưu lại trạng thái

--- be/clinic/src/main/java/com/clinic/clinic/controller/DoctorController.java
+++ be/clinic/src/main/java/com/clinic/clinic/controller/DoctorController.java
@@ -11,9 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/doctors")
